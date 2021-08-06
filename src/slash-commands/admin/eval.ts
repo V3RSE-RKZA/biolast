11de325811403bad4f594981a91ba40cf9a2e69f
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-08-06 14:03:15 -0400

    eval slash command

---
+++ src/slash-commands/admin/eval.ts
@@ -0,0 +1,87 @@
+import { CommandOptionType, SlashCreator, CommandContext } from 'slash-create'
+import { inspect } from 'util'
+import App from '../../app'
+import { allLocations } from '../../resources/raids'
+import CustomSlashCommand from '../../structures/CustomSlashCommand'
+import Embed from '../../structures/Embed'
+import { query } from '../../utils/db/mysql'
+
+class EvalCommand extends CustomSlashCommand {
+	constructor (creator: SlashCreator, app: App) {
+		super(creator, app, {
+			name: 'eval',
+			description: 'Eval stuff...',
+			options: [{
+				// TODO update this once discord releases full text input
+				type: CommandOptionType.STRING,
+				name: 'input',
+				description: 'yes.',
+				required: true
+			}],
+			category: 'admin',
+			guildModsOnly: false,
+			worksInDMs: true,
+			onlyWorksInRaidGuild: false,
+			canBeUsedInRaid: true,
+			guildIDs: ['497302646521069568'],
+			deferEphemeral: true
+		})
+
+		this.filePath = __filename
+	}
+
+	async run (ctx: CommandContext): Promise<void> {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const test = {
+			allLocations,
+			query
+		}
+		let commandInput = ctx.options.input
+
+		if (commandInput.startsWith('```')) {
+			// remove the first and last lines from code block
+			commandInput = commandInput.split('\n').slice(1, -1).join('\n')
+		}
+
+		try {
+			const start = new Date().getTime()
+			// eslint-disable-next-line no-eval
+			let evaled = await eval(commandInput)
+			const end = new Date().getTime()
+
+			if (typeof evaled !== 'string') evaled = inspect(evaled)
+
+			const segments = evaled.match(/[\s\S]{1,1500}/g)
+
+			if (segments.length === 1) {
+				const evalEmbed = new Embed()
+					.setDescription(`\`\`\`js\n${segments[0]}\`\`\``)
+					.setColor(12118406)
+					.setFooter(`${end - start}ms`)
+
+				await ctx.send({
+					embeds: [evalEmbed.embed]
+				})
+			}
+			else {
+				for (let i = 0; i < (segments.length < 5 ? segments.length : 5); i++) {
+					await ctx.send({
+						content: `\`\`\`js\n${segments[i]}\`\`\``
+					})
+				}
+			}
+		}
+		catch (err) {
+			const evalEmbed = new Embed()
+				.setTitle('Something went wrong.')
+				.setDescription(`\`\`\`js\n${err}\`\`\``)
+				.setColor(13914967)
+
+			await ctx.send({
+				embeds: [evalEmbed.embed]
+			})
+		}
+	}
+}
+
+export default EvalCommand
