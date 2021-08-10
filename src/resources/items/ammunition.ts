6f9ba49e4941c0d84ff5ee42d4a2df0fa1e58bd0
FireBoy69 <88595833+FireBoy69@users.noreply.github.com>
2021-08-10 21:53:51 +0100

    Update ammunition.ts

--- src/resources/items/ammunition.ts
+++ src/resources/items/ammunition.ts
@@ -63,5 +63,17 @@ export const items = ammoObject({
 		ammoFor: [ranged.makeshift_shotgun],
 		sellPrice: 500,
 		slotsUsed: 1
-	}
+	},
+        '.303_bullet': {
+		type: 'Ammunition',
+		name: '.303_bullet',
+		icon: '<:U_ammo:601366669318815745>',
+		aliases: ['.303']
+		description: 'A handmade shotgun shell filled with cheap gunpowder and pellets.',
+		damage: 45,
+		penetration: 4.5
+		ammoFor: [ranged.lee-enfield],
+		sellPrice: 1100,
+		slotsUsed: 1
+        },
 })
