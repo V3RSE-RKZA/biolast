db8bd2a04d48fa28e6175dd86f7c5109692e47e7
TheRealestBird <88551833+TheRealestBird@users.noreply.github.com>
2021-09-07 00:49:35 -0400

    Add bone_armor (#72)

--- src/resources/items/armor.ts
+++ src/resources/items/armor.ts
@@ -18,7 +18,7 @@ export const items = armorObject({
 		name: 'cloth_armor',
 		icon: '<:U_shield:601366669474136074>',
 		aliases: ['cloth', 'armor'],
-		sellPrice: 1000,
+		sellPrice: 750,
 		durability: 3,
 		level: 1,
 		slotsUsed: 2
@@ -28,19 +28,19 @@ export const items = armorObject({
 		name: 'wooden_armor',
 		icon: '<:U_shield:601366669474136074>',
 		aliases: ['wood_armor'],
-		sellPrice: 1500,
+		sellPrice: 900,
 		durability: 3,
 		level: 2,
 		slotsUsed: 2
 	},
-	metal_chestplate: {
+	bone_armor: {
 		type: 'Body Armor',
-		name: 'metal_chestplate',
+		name: 'bone_armor',
 		icon: '<:U_shield:601366669474136074>',
-		aliases: ['chestplate'],
-		sellPrice: 3000,
-		durability: 5,
-		level: 5,
+		aliases: ['bone'],
+		sellPrice: 2000,
+		durability: 8,
+		level: 2,
 		slotsUsed: 3
 	}
 })
