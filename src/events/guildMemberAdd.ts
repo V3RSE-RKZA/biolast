f56877d06b52f048e157d402f8b72e8a096f5ead
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-09-06 21:20:22 -0400

    add random nickname to users in raid server
    this helps prevent issues with unpingable names

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -3,9 +3,17 @@ import { query } from '../utils/db/mysql'
 import { getUsersRaid } from '../utils/db/raids'
 import { logger } from '../utils/logger'
 import { isRaidGuild } from '../utils/raidUtils'
+import getRandomInt from '../utils/randomInt'
 
 export async function run (guild: Guild, member: Member): Promise<void> {
 	if (isRaidGuild(guild.id)) {
+		try {
+			await member.edit({ nick: `Scavenger ${getRandomInt(10000, 99999)}` }, 'Setting random scavenger nickname')
+		}
+		catch (err) {
+			logger.err('Failed to change members nickname in raid guild:', err)
+		}
+
 		const userRaid = await getUsersRaid(query, member.id)
 
 		if (userRaid && userRaid.guildId === guild.id) {
