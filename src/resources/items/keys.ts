4e3004db61d67e3daf8726f3b8984484b0a838b9
EriisGay <90113693+EriisGay@users.noreply.github.com>
2021-09-09 17:00:03 -0500

    Add warehouse_key and truck_key (#88)
    * Add two new key items
    
    Warehouse key for openable area in farm, and truck key for evac route in farm.
    
    * [Linter] Apply linters fixes
    
    * Update src/resources/items/keys.ts
    
    Co-authored-by: blobfysh <47480729+blobfysh@users.noreply.github.com>
    
    * Update src/resources/items/keys.ts
    
    Co-authored-by: blobfysh <47480729+blobfysh@users.noreply.github.com>
    
    Co-authored-by: EriisGay <EriisGay@users.noreply.github.com>
    Co-authored-by: blobfysh <47480729+blobfysh@users.noreply.github.com>

--- src/resources/items/keys.ts
+++ src/resources/items/keys.ts
@@ -11,5 +11,23 @@ export const items = keysObject({
 		icon: '<:U_key:870786870852874260>',
 		slotsUsed: 3,
 		itemLevel: 1
+	},
+	warehouse_key: {
+		type: 'Key',
+		name: 'warehouse_key',
+		durability: 3,
+		aliases: ['wh_key', 'warehouse'],
+		icon: '<:U_key:870786870852874260>',
+		slotsUsed: 3,
+		itemLevel: 3
+	},
+	truck_key: {
+		type: 'Key',
+		name: 'truck_key',
+		durability: 1,
+		aliases: ['truck'],
+		icon: '<:U_key:870786870852874260>',
+		slotsUsed: 2,
+		itemLevel: 3
 	}
 })
