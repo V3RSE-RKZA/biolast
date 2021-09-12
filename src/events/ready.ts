70e8a8c3ab3fd7372448a6857fa8a3687769bdc6
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-09-12 01:01:37 -0400

    automatic raid guild syncing
    raid guilds will be automatically structured, all you need to do is provide the guild ID in the .env file and invite the bot with admin perms.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,16 @@
 import App from '../app'
 import { logger } from '../utils/logger'
+import { syncRaidGuilds } from '../utils/syncRaidGuilds'
 
 export async function run (this: App): Promise<void> {
-	logger.info('Bot ready!')
+	// sync raid guilds with the proper channel structure and permissions
+	await syncRaidGuilds(this)
 
 	await this.loadRaidTimers()
+
+	await new Promise(res => setTimeout(res, 1500))
 	await this.npcHandler.start()
 
 	this.acceptingCommands = true
+	logger.info('Bot ready and accepting commands!')
 }
