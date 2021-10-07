fadcd38d76aebd0c4edc81f229f7ef588d560d87
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-10-07 12:52:42 -0400

    kick users from raid whos raid expired while bot was offline

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,7 @@
 import App from '../app'
+import { allLocations } from '../resources/raids'
+import { query } from '../utils/db/mysql'
+import { userInRaid } from '../utils/db/raids'
 import { logger } from '../utils/logger'
 import { syncRaidGuilds } from '../utils/syncRaidGuilds'
 
@@ -10,6 +13,25 @@ export async function run (this: App): Promise<void> {
 
 	await new Promise(res => setTimeout(res, 1500))
 	await this.npcHandler.start()
+
+	// kick out players who are still in a raid even though their raid timer expired while bot was offline
+	for (const location of allLocations) {
+		for (const id of location.guilds) {
+			const guild = this.bot.guilds.get(id)
+
+			if (guild) {
+				await guild.fetchAllMembers()
+
+				for (const mem of guild.members) {
+					const member = mem[1]
+
+					if (member.id !== guild.ownerID && member.id !== this.bot.user.id && !(await userInRaid(query, member.id))) {
+						await member.kick('Users raid has expired while the bot was offline')
+					}
+				}
+			}
+		}
+	}
 
 	this.acceptingCommands = true
 	logger.info('Bot ready and accepting commands!')
