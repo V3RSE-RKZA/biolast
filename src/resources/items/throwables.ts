8f92d6fc83cf70b484dddc08556aac4a43e4a814
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-10-30 01:20:20 -0400

    add f1_grenade and m67_grenade

--- src/resources/items/throwables.ts
+++ src/resources/items/throwables.ts
@@ -3,10 +3,10 @@ import { ThrowableWeapon } from '../../types/Items'
 const throwablesObject = <T>(et: { [K in keyof T]: ThrowableWeapon & { name: K } }) => et
 
 export const throwables = throwablesObject({
-	grenade: {
+	F1_grenade: {
 		type: 'Throwable Weapon',
 		subtype: 'Fragmentation Grenade',
-		name: 'grenade',
+		name: 'F1_grenade',
 		icon: '<:U_melee:601366669251575857>',
 		aliases: [],
 		sellPrice: 476,
@@ -17,6 +17,22 @@ export const throwables = throwablesObject({
 		damage: 54,
 		penetration: 1.7,
 		itemLevel: 8,
+		spreadsDamageToLimbs: 3
+	},
+	M67_grenade: {
+		type: 'Throwable Weapon',
+		subtype: 'Fragmentation Grenade',
+		name: 'M67_grenade',
+		icon: '<:U_melee:601366669251575857>',
+		aliases: [],
+		sellPrice: 2025,
+		durability: 1,
+		slotsUsed: 1,
+		fireRate: 45,
+		accuracy: 74,
+		damage: 57,
+		penetration: 3.1,
+		itemLevel: 12,
 		spreadsDamageToLimbs: 3
 	}
 })
