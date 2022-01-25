a29848c41f760c61d6ded734f80b712aa72e6bab
blobfysh <47480729+blobfysh@users.noreply.github.com>
2022-01-24 22:57:57 -0500

    add to help faq

--- src/slash-commands/help.ts
+++ src/slash-commands/help.ts
@@ -11,20 +11,24 @@ import { logger } from '../utils/logger'
 import { disableAllComponents } from '../utils/messageUtils'
 import { combineArrayWithOr } from '../utils/stringUtils'
 
+const waveID = icons.wave.match(/:([0-9]*)>/)
 const faq: { question: string, answer: string }[] = [
 	{
-		question: '',
-		answer: ''
+		question: 'How do I play/progress?',
+		answer: 'You need to scavenge your region for weapons, armor, and other equipment that could help you survive.' +
+			' **To start scavenging, use the `/scavenge` command**. You\'ll be prompted to choose which area in your region you want to scavenge.' +
+			' Some areas are patrolled by mobs such as walkers that you\'ll need to kill before you can search that area. **If you\'re' +
+			' just starting out, you should stick to the areas that aren\'t guarded so that you can gear up**.\n\nOnce you think you have enough' +
+			' gear from scavenging, you can attempt the **boss fight**. Boss fights allow you to team with up to **2** friends to take down a' +
+			' powerful mob. **By defeating the boss, you and your friends will unlock a new region to explore**. The regions you unlock will' +
+			' have better weapons and gear, as well as more powerful mobs to fight. The boss of each area is different and will require you' +
+			' to plan the fight. Use `/boss` to attempt the fight.\n\nFighting bosses and scavenging are the main ways to get gear, but you could also progress by using outside sources' +
+			' such as a `/companion`, the `/market`, trading with other players, or the `/merchant`.'
 	},
 	{
-		question: 'How is damage calculated?',
-		answer: 'Weapons are classified as either melee or ranged. If your weapon is melee, you can view how much damage it does by checking the item stats: `/item <item name>`' +
-			'\n\nIf however you are trying to check how much damage a ranged weapon will deal, you\'ll need to instead check how much damage the **ammo** your using deals. This is because **damage with ranged weapons' +
-			' is entirely dependent on the ammo used**.\n\nArmor may also affect how much damage you deal, **if the ammo your using has a lower penetration level than the armor level the target is wearing, your damage will' +
-			' be reduced**. If your ammo has a higher penetration level than the armor level your target is wearing, you will deal full damage.\n\nHitting certain body parts will also determine your damage. A hit to the target\'s head' +
-			' will deal 1.5x damage (assuming they aren\'t wearing a helmet) but is obviously harder to hit. A hit to the chest will deal normal damage and is the easiest body part to hit.' +
-			' A hit to the arms or legs deals half damage but you also avoid hitting any armor the target is wearing. **Your ability to successfully hit a targeted limb is dependent on your' +
-			' weapon\'s accuracy.**'
+		question: 'How do I level up?',
+		answer: `You will gain ${icons.xp_star} **XP** when you scavenge, win fights, or complete quests. **Completing quests is the fastest way to gain XP**.` +
+			'\n\nYou can view your level progress by using `/level`, `/profile`, or `/inventory`.'
 	},
 	{
 		question: 'How do I heal?',
@@ -36,6 +40,26 @@ const faq: { question: string, answer: string }[] = [
 		answer: 'You can equip a backpack to increase your inventory space. Backpacks of varying levels can be found from scavenging or bought from the shop.' +
 			' Once you have one, simply do `/equip <item id>` to equip the backpack.' +
 			'\n\nStash space cannot be altered with backpacks, instead stash space will increase by **5** each time you level up.'
+	},
+	{
+		question: 'Any tips for winning fights?',
+		answer: 'If the mob you are planning to fight is wearing armor, be sure to check how good the armor is with `/item info <item name>` and verify' +
+			' that the weapon you have will penetrate their armor or at least get close. Armor drastically reduces damage from weapons with a low penetration level.' +
+			'\n\nIf you plan on healing during a 1v1 fight, make sure the medical item you are going to use heals for at least more damage than the mob/player will hit you for.' +
+			' It\'s pointless to heal using a medical item that doesn\'t heal more than their damage because you will still lose health when they attack you' +
+			' (this tip only applies to 1v1 duels, not boss fights where your friends are helping you).' +
+			'\n\nIf you plan on using a stimulant, use it at the very start of a fight. Stimulants last the entire duel, so using it after a few turns' +
+			' would be a waste.'
+	},
+	{
+		question: 'Complex - How is damage calculated?',
+		answer: 'Weapons are classified as either melee or ranged. If your weapon is melee, you can view how much damage it does by checking the item stats: `/item <item name>`' +
+			'\n\nIf however you are trying to check how much damage a ranged weapon will deal, you\'ll need to instead check how much damage the **ammo** your using deals. This is because **damage with ranged weapons' +
+			' is entirely dependent on the ammo used**.\n\nArmor may also affect how much damage you deal, **if the ammo your using has a lower penetration level than the armor level the target is wearing, your damage will' +
+			' be reduced**. If your ammo has a higher penetration level than the armor level your target is wearing, you will deal full damage.\n\nHitting certain body parts will also determine your damage. A hit to the target\'s head' +
+			' will deal 1.5x damage (assuming they aren\'t wearing a helmet) but is obviously harder to hit. A hit to the chest will deal normal damage and is the easiest body part to hit.' +
+			' A hit to the arms or legs deals half damage but you also avoid hitting any armor the target is wearing. **Your ability to successfully hit a targeted limb is dependent on your' +
+			' weapon\'s accuracy.** If your weapon has less than **50%** accuracy, you won\'t be able to target a limb.'
 	}
 ]
 
@@ -149,12 +173,16 @@ class HelpCommand extends CustomSlashCommand {
 								{
 									label: 'What are the commands?',
 									value: 'commands',
-									description: ''
+									description: '',
+									emoji: waveID ? {
+										id: waveID[1],
+										name: 'wave'
+									} : undefined
 								},
 								...faq.map(q => ({
 									label: q.question,
 									value: q.question,
-									description: ''
+									description: `${q.answer.replace(/<a?:.*:(\d+)>?|((_|\*|~|\|){2})|`/g, '').slice(0, 40)}…`
 								}))
 							]
 						}
@@ -172,7 +200,35 @@ class HelpCommand extends CustomSlashCommand {
 				if (c.values.includes('commands')) {
 					await c.editParent({
 						content: '',
-						embeds: [commandsEmb.embed]
+						embeds: [commandsEmb.embed],
+						components: [
+							{
+								type: ComponentType.ACTION_ROW,
+								components: [
+									{
+										type: ComponentType.SELECT,
+										custom_id: 'help-command',
+										placeholder: 'Frequently Asked Questions:',
+										options: [
+											{
+												label: 'What are the commands?',
+												value: 'commands',
+												description: '',
+												emoji: waveID ? {
+													id: waveID[1],
+													name: 'wave'
+												} : undefined
+											},
+											...faq.map(q => ({
+												label: q.question,
+												value: q.question,
+												description: `${q.answer.replace(/<a?:.*:(\d+)>?|((_|\*|~|\|){2})|`/g, '').slice(0, 40)}…`
+											}))
+										]
+									}
+								]
+							}
+						]
 					})
 				}
 				else {
@@ -185,7 +241,35 @@ class HelpCommand extends CustomSlashCommand {
 
 						await c.editParent({
 							content: '',
-							embeds: [qEmbed.embed]
+							embeds: [qEmbed.embed],
+							components: [
+								{
+									type: ComponentType.ACTION_ROW,
+									components: [
+										{
+											type: ComponentType.SELECT,
+											custom_id: 'help-command',
+											placeholder: 'Frequently Asked Questions:',
+											options: [
+												{
+													label: 'What are the commands?',
+													value: 'commands',
+													description: ''
+												},
+												...faq.map(q => ({
+													label: q.question,
+													value: q.question,
+													description: `${q.answer.replace(/<a?:.*:(\d+)>?|((_|\*|~|\|){2})|`/g, '').slice(0, 40)}…`,
+													emoji: waveID && faqQuestion.question === q.question ? {
+														id: waveID[1],
+														name: 'wave'
+													} : undefined
+												}))
+											]
+										}
+									]
+								}
+							]
 						})
 					}
 				}
