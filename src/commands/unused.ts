3355691d069ee2fdef2f4623893020ccb676d09e
blobfysh <47480729+blobfysh@users.noreply.github.com>
2022-01-31 23:37:35 -0500

    fix unused command

--- src/commands/unused.ts
+++ src/commands/unused.ts
@@ -4,6 +4,7 @@ import { allLocations } from '../resources/locations'
 import { getItemDisplay, sortItemsByType } from '../utils/itemUtils'
 import { allItems } from '../resources/items'
 import { Item } from '../types/Items'
+import { merchantTrades } from '../resources/trades'
 
 // yes this command is ugly its only for admins >:(
 const ITEMS_PER_PAGE = 20
@@ -16,32 +17,62 @@ export const command: TextCommand = {
 	async execute (app, message, { args, prefix }) {
 		const obtainable: Item[] = []
 
-		for (const item of allItems) {
+		for (const itemFixed of allItems) {
 			for (const loc of allLocations) {
-				for (const chan of loc.areas) {
-					if (chan.loot) {
-						if (chan.loot.common.items.find(i => i.name === item.name)) {
-							obtainable.push(item)
-						}
-						else if (chan.loot.uncommon.items.find(i => i.name === item.name)) {
-							obtainable.push(item)
-						}
-						else if (chan.loot.rare.items.find(i => i.name === item.name)) {
-							obtainable.push(item)
-						}
-						else if (chan.loot.rarest?.items.find(i => i.name === item.name)) {
-							obtainable.push(item)
-						}
-
-						if (chan.requiresKey && chan.keyIsOptional && chan.specialLoot.items.find(i => i.name === item.name)) {
-							obtainable.push(item)
-						}
+				for (const area of loc.areas) {
+					if (area.loot.common.items.find(i => i.name === itemFixed.name)) {
+						obtainable.push(itemFixed)
 					}
+					else if (area.loot.uncommon.items.find(i => i.name === itemFixed.name)) {
+						obtainable.push(itemFixed)
+					}
+					else if (area.loot.rare.items.find(i => i.name === itemFixed.name)) {
+						obtainable.push(itemFixed)
+					}
+					else if (area.loot.rarest?.items.find(i => i.name === itemFixed.name)) {
+						obtainable.push(itemFixed)
+					}
+					else if (area.requiresKey && area.keyIsOptional && area.specialLoot.items.find(i => i.name === itemFixed.name)) {
+						obtainable.push(itemFixed)
+					}
+
+					if (
+						area.npc &&
+						(
+							(area.npc.armor && area.npc.armor.name === itemFixed.name) ||
+							(area.npc.helmet && area.npc.helmet.name === itemFixed.name) ||
+							(area.npc.type === 'raider' && area.npc.weapon.name === itemFixed.name) ||
+							(area.npc.type === 'raider' && 'ammo' in area.npc && area.npc.ammo.name === itemFixed.name) ||
+							(area.npc.drops.common.find(i => i.name === itemFixed.name)) ||
+							(area.npc.drops.uncommon.find(i => i.name === itemFixed.name)) ||
+							(area.npc.drops.rare.find(i => i.name === itemFixed.name))
+						)
+					) {
+						obtainable.push(itemFixed)
+					}
+				}
+
+				if (
+					(loc.boss.armor && loc.boss.armor.name === itemFixed.name) ||
+					(loc.boss.helmet && loc.boss.helmet.name === itemFixed.name) ||
+					(loc.boss.type === 'raider' && loc.boss.weapon.name === itemFixed.name) ||
+					(loc.boss.type === 'raider' && 'ammo' in loc.boss && loc.boss.ammo.name === itemFixed.name) ||
+					(loc.boss.drops.common.find(i => i.name === itemFixed.name)) ||
+					(loc.boss.drops.uncommon.find(i => i.name === itemFixed.name)) ||
+					(loc.boss.drops.rare.find(i => i.name === itemFixed.name))
+				) {
+					obtainable.push(itemFixed)
+				}
+			}
+
+			for (const trade of merchantTrades) {
+				if (trade.offer.item.name === itemFixed.name) {
+					obtainable.push(trade.offer.item)
 				}
 			}
 		}
 
-		const unobtainables = sortItemsByType(allItems.filter(itm => !obtainable.includes(itm)))
+		const unobtainables = sortItemsByType(allItems.filter(itm => itm.name !== 'dog_tags' && !obtainable.includes(itm)))
 		const pages = generatePages(unobtainables)
 
 		for (const page of pages) {
